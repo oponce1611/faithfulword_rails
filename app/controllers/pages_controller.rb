c154eaced95458926de4d79741c788d00838ad20
Osmar Ponce <krekrre14@gmail.com>
2017-03-06 17:57:55 +0000

    new pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,57 @@
+class PagesController < ApplicationController
+  def audiobible
+    render "audiobible"
+  end
+  
+  def biblememory
+    
+  end
+  
+  def churchdirectory
+    
+  end
+  
+  def doctrinalstatement
+    
+  end
+  
+  def events
+    render "events"
+  end
+  
+  def location
+    
+  end
+  
+  def malawi
+    
+  end
+  
+  def ourpastor
+    
+  end
+  
+  def privacypolicy
+    
+  end
+  
+  def psalms
+    
+  end
+  
+  def radio
+    
+  end
+  
+  def service
+    
+  end
+  
+  def soulwinning
+    
+  end
+  
+  def termsandconditions
+    
+  end
+end
