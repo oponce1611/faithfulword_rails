3181fffc1606be5c1143368b7020dec3ee27196f
Osmar Ponce <krekrre14@gmail.com>
2017-03-06 18:03:23 +0000

    fixed linkto

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,15 +4,15 @@ class PagesController < ApplicationController
   end
   
   def biblememory
-    
+    render "biblememory"
   end
   
   def churchdirectory
-    
+    render "churchdirectory"
   end
   
   def doctrinalstatement
-    
+    render "doctrinalstatement"
   end
   
   def events
@@ -20,38 +20,38 @@ class PagesController < ApplicationController
   end
   
   def location
-    
+    render "location"
   end
   
   def malawi
-    
+    render "malawi"
   end
   
   def ourpastor
-    
+    render "ourpastor"
   end
   
   def privacypolicy
-    
+    render "privacypolicy"
   end
   
   def psalms
-    
+    render "psalms"
   end
   
   def radio
-    
+    render "radio"
   end
   
   def service
-    
+    render "service"
   end
   
   def soulwinning
-    
+    render "soulwinning"
   end
   
   def termsandconditions
-    
+    render "termsandconditions"
   end
 end
