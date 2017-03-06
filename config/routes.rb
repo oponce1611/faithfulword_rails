96e6248adbefc2b547ad00be489e55927581df83
Osmar Ponce <krekrre14@gmail.com>
2017-03-06 19:26:19 +0000

    asdasd

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   get '/malawi' => 'pages#malawi'
   get '/ourpastor' => 'pages#ourpastor'
   get '/preaching' => 'preaching#show'
-  get '/privacypolicy' => 'pages#preaching'
+  get '/privacypolicy' => 'pages#privacypolicy'
   get '/psalms' => 'pages#psalms'
   get '/radio' => 'pages#radio'
   get '/service' => 'pages#service'
