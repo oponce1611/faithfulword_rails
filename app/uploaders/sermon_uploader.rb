107d693d976fd45e214b04235620c975b04e2efa
Osmar Ponce <krekrre14@gmail.com>
2017-04-19 18:52:19 +0000

    Aded rename functionalty for sermon uploads

--- app/uploaders/sermon_uploader.rb
+++ app/uploaders/sermon_uploader.rb
@@ -7,4 +7,20 @@ class SermonUploader < CarrierWave::Uploader::Base
   def extension_whitelist
     %w(mp3)
   end
+  
+  def filename
+    "#{rename(model.title, model.datetime)}.#{file.extension}" if original_filename.present?
+  end
+  
+  protected
+  
+  def rename(title, date)
+    title = title.gsub('&', "and")
+    title =  title.gsub(/[!:?@%.,'"]/,'')
+    title = title.split.map { |i| i.capitalize }.join
+    date = date.strftime("%Y%m%d%p").chomp 'M'
+    file_name = date + "_" + title
+    file_name
+  end
+
 end
